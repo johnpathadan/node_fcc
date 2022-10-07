c87e7b2f71e4b6d8928c132ba1942e8b15c381b9
johnpathadan <johnpathadan@gmail.com>
2022-10-07 19:21:56 +0530

    fix: alternative module exports

---
+++ 5-alternitive-way.js
@@ -0,0 +1,10 @@
+//different ways of exporting modules
+
+// const items = ["item 1", "item 2"];
+module.exports.items = ["item 1", "item 2"];
+
+const user = {
+  name: "Benjamin",
+};
+
+module.exports.person = user;
--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const names = require("./3-names");
-
-console.log(names);
-
 const sayHi = require("./4-utils");
+const data = require("./5-alternitive-way");
+
+console.log(data);
+//{ items: [ 'item 1', 'item 2' ], person: { name: 'Benjamin' } }
 sayHi("Sara");
 sayHi(names.john);
 sayHi(names.peter);
