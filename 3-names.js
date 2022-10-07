83e7665d5cad5f42f6ad7fa4515224b97701f155
johnpathadan <johnpathadan@gmail.com>
2022-10-07 18:59:33 +0530

    fix: module implementation

---
+++ 3-names.js
@@ -0,0 +1,9 @@
+//local
+const secret = "SUPER SECRET";
+//share with rest of the application
+const john = "John";
+const peter = "Peter";
+
+console.log(module);
+
+module.exports = { john, peter };
--- app.js
+++ app.js
@@ -1,12 +1,8 @@
-//Here are some of the variables in nodejs
-//GLOBALS - NO WINDOW - You can access it anywhere
-//__dirname - Path to current directory
-//__filename - file name
-//require - function to use modules (CommonJS)
-//module - info about current module(file)
-//process - info about environment where the program is being executed(very useful)(heroku, digital ocean etc..)
+const names = require("./3-names");
 
-console.log(__dirname);
-setInterval(() => {
-  console.log("Hello World");
-}, 1000);
+console.log(names);
+
+const sayHi = require("./4-utils");
+sayHi("Sara");
+sayHi(names.john);
+sayHi(names.peter);
